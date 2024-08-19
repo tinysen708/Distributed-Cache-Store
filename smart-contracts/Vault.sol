4797385d9ed60851518c90360f41acd221e53613
tinysen708 <265719315+tinysen708@users.noreply.github.com>
2024-08-19 13:21:49 +0700

    perf: reduce memory footprint in Vault.sol

--- smart-contracts/Vault.sol
+++ smart-contracts/Vault.sol
@@ -41,3 +41,4 @@ contract EnterpriseYieldVault is ReentrancyGuard, Ownable {
 // Optimized logic batch 6463
 // Optimized logic batch 1341
 // Optimized logic batch 5914
+// Optimized logic batch 6151
